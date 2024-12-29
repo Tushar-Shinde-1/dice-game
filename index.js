ae0e21661b03437f0794734f3788048eaa4e5dd3
Shinde Tushar <tusharshinde5232@gmail.com>
2024-12-29 08:54:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var random1=Math.floor(Math.random()*6)+1;
+var assign="images/" + "dice"+random1+".png";
+document.querySelectorAll("img")[0].setAttribute("src",assign);
+
+var random2=Math.floor(Math.random()*6)+1;
+var assign1="images/dice"+random2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",assign1);
+if(random1>random2)
+{
+    document.querySelectorAll("h1")[0].innerHTML="player 1 won ";
+}
+else if(random1==random2)
+{
+document.querySelectorAll("h1")[0].innerHTML="tie ";
+
+}
+else{
+    document.querySelector("h1").innerHTML="player 2 win";
+}
